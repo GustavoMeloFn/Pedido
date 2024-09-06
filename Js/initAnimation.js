804c056e7583b309cdf68edbde03fc8d6d6be1df
Gustavo Melo <gustavo.melo.fn@gmail.com>
2024-09-06 20:22:26 -0300

    doguinho fofo

--- Js/initAnimation.js
+++ Js/initAnimation.js
@@ -1,4 +1,12 @@
 onload = () => {
+
+    Swal.fire({
+      imageUrl: "../img/dogFofo.jpg",
+      imageHeight: 250,
+      imageAlt: "A tall image",
+      text: "Hehehe Não pensei que chegaria até aqui, aceite isso:"
+    });
+
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
